23ffe26542afe5c1033324f81f5fb1713e4b7732
Ted Williams <williamsted1001@gmail.com>
2026-03-12 13:39:51 +0300

    merged changes

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,7 +4,7 @@ import path from "node:path";
 // Set up multer storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
-        cb(null, "uploads/"); // specify the destination directory for uploaded files
+        cb(null, "./uploads/"); // specify the destination directory for uploaded files
     },
     filename: (req, file, cb)=>{
         cb(null, Date.now()+ path.extname(file.originalname)); // specify the filename for uploaded files
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 }
 }))
 
 
